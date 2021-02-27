b6c4a27a0ed891ca969568b36176bf7c15881083
andreitecsor <andrei.tecsor@gmail.com>
2021-02-27 12:53:57 +0200

    Main created

--- src/Sem1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package Sem1;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World - CTS");
-    }
-}
---
+++ src/Sem1/Main.java
@@ -0,0 +1,9 @@
+package Sem1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World - CTS");
+        Person person = new Person("Chuck", "Norris");
+        System.out.println(person.toString());
+    }
+}
