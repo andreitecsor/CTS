3c99eb106b97ba11f6c36b1c54017e2ff2bef329
andreitecsor <andrei.tecsor@gmail.com>
2021-03-04 13:50:53 +0200

    added CalendarUtil

---
+++ src/Sem2/Main.java
@@ -0,0 +1,8 @@
+package Sem2;
+
+public class Main {
+    public static void main(String[] args) {
+        CalendarUtil cu = new CalendarUtil();
+        System.out.println(cu.weekDay(3));
+    }
+}
