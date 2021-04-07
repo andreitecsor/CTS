bb82caa9bfdcd316fb3ad59f2dc629f4ddd26891
andreitecsor <andrei.tecsor@gmail.com>
2021-04-07 14:43:03 +0300

    TestSimpleFactory bug fixed

--- src/seminar/sem7/simplefactory/TestSimpleFactory.java
+++ src/seminar/sem7/simplefactory/TestSimpleFactory.java
@@ -2,13 +2,13 @@ package seminar.sem7.simplefactory;
 
 public class TestSimpleFactory {
     public static void main(String[] args) {
-        SuperErouAbstract supererou = new CaracterDisney("Mickey Mouse", 100, false);
-        supererou = new CaracterMarvel("Spider-man", 150, 50);
-        supererou = new CaracterDCComics("Batman", 150, 100);
+//        SuperErouAbstract supererou = new CaracterDisney("Mickey Mouse", 100, false);
+//        supererou = new CaracterMarvel("Spider-man", 150, 50);
+//        supererou = new CaracterDCComics("Batman", 150, 100);
 
-        SuperErouAbstract superCaracter = FactorySuperErou.getSuperErou(TipErou.DISNEY, "Mickey Mouse");
-        superCaracter = FactorySuperErou.getSuperErou(TipErou.MARVEL, "Spider-man");
-        superCaracter = FactorySuperErou.getSuperErou(TipErou.DISNEY, "Batman");
+        SuperErouAbstract superCaracter1 = FactorySuperErou.getSuperErou(TipErou.DISNEY, "Mickey Mouse");
+        SuperErouAbstract superCaracter2 = FactorySuperErou.getSuperErou(TipErou.MARVEL, "Spider-man");
+        superCaracter2 = FactorySuperErou.getSuperErou(TipErou.DC, "Batman");
 
     }
 }
