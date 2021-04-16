ac89749301bca981b63f3401b785bc09ae8466fc
andreitecsor <andrei.tecsor@gmail.com>
2021-04-16 10:33:36 +0300

    Composite done

---
+++ src/seminar/sem5/recuperare/composite/InterfataCaractereNPC.java
@@ -0,0 +1,10 @@
+package seminar.sem5.recuperare.composite;
+
+public interface InterfataCaractereNPC {
+
+    public void atacaJucator(String numeJucator);
+
+    public void seRetrage();
+
+    public void seDeplaseaza();
+}
