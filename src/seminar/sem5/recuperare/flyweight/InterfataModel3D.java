eef32a23bf975459aefd0de7f9119010e66a4163
andreitecsor <andrei.tecsor@gmail.com>
2021-04-16 11:24:40 +0300

    Flyweight done

---
+++ src/seminar/sem5/recuperare/flyweight/InterfataModel3D.java
@@ -0,0 +1,8 @@
+package seminar.sem5.recuperare.flyweight;
+
+public interface InterfataModel3D {
+
+    public void incarcaModel3D(String fisier);
+
+    public void afisareEcran(ContextEcran context);
+}
