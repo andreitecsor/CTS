a68a4b4aa1bc68d48eafae1a6a4891d5eb2aae8e
andreitecsor <andrei.tecsor@gmail.com>
2021-05-12 14:24:53 +0300

    adaugare manager command

---
+++ src/seminar/sem10/command/ManagerTaskuri.java
@@ -0,0 +1,20 @@
+package seminar.sem10.command;
+
+import java.util.ArrayList;
+
+public class ManagerTaskuri {
+    ArrayList<TaskAsincron> taskuri = new ArrayList<>();
+
+    public void adaugaTask(TaskAsincron task) {
+        taskuri.add(task);
+    }
+
+    public void executaTask(){
+        if(taskuri.size() > 0){
+            TaskAsincron task = taskuri.get(0);
+            task.executaTask();
+            taskuri.remove(0);
+        }
+    }
+
+}
--- src/seminar/sem10/command/TestCommand.java
+++ src/seminar/sem10/command/TestCommand.java
@@ -2,11 +2,20 @@ package seminar.sem10.command;
 
 public class TestCommand {
     public static void main(String[] args) {
+        ManagerTaskuri managerTaskuri = new ManagerTaskuri();
+
         TaskAsincron autoSave = new TaskToDo(new ModulAutoSave(), "Autosave", 5);
         TaskAsincron autoUpdate = new TaskToDo(new ModulUpdateClient(), "Update v2", 20);
 
+        managerTaskuri.adaugaTask(autoSave);
+        managerTaskuri.adaugaTask(autoUpdate);
+        System.out.println("Jocul este activ");
+        managerTaskuri.executaTask();
+        managerTaskuri.executaTask();
+        System.out.println("Jocul este activ");
+
         //executie task-uri asincrone
-        autoSave.executaTask();
-        autoUpdate.executaTask();
+//        autoSave.executaTask();
+//        autoUpdate.executaTask();
     }
 }
